d9c1049dd37b7f5e83ef6964a8459963f80a3ab4
GeorgiyI <izmailov.georgiy.fortech@gmail.com>
2022-08-17 14:34:07 +0300

    minor fixes

--- src/__tests__/storageTester.test.ts
+++ src/__tests__/storageTester.test.ts
@@ -14,7 +14,7 @@ const testConstants = {
 
 const testFilePath = `${__dirname}/mockFiles/file.${testConstants.extension}`; // Path to test file
 describe('Middleware', () => {
-  test('FileMiddleware: \n', async () => {
+  test('FileMiddleware', async () => {
     /* Mock request containing required fields for testing */
     const mockRequest = {
       headers: {
@@ -36,6 +36,7 @@ describe('Middleware', () => {
     }) as NextFunction);
   });
 });
+
 describe('Storage adapter', () => {
   test('getFile', () => {
     const storage = FileStorageGetter.getStorage();
@@ -47,6 +48,7 @@ describe('Storage adapter', () => {
 
   test('saveFile', () => {
     const storage = FileStorageGetter.getStorage();
+
     const filename = 'otjYQU1lDdCjONSvTUGmuuUUacpTEooF';
     const file = fs.readFileSync(`${__dirname}/mockFiles/file.${testConstants.extension}`);
     const metadata = { content_type: testConstants.content_type, content_length: testConstants.content_length };
@@ -58,10 +60,10 @@ describe('Storage adapter', () => {
           throw new Error(`${key} of response from storage.saveFile() was null or undefined, which shouldn't happen `);
       });
 
+      const serverRoot = Server.rootFolder.substring(0, Server.rootFolder.length - 4);
+
       /* Expected uploaded file path */
-      const expectedPath = `${Server.rootFolder.substring(0, Server.rootFolder.length - 4)}/${
-        config.storage.localSavePath
-      }${filename}.${testConstants.extension}`;
+      const expectedPath = `${serverRoot}/${config.storage.localSavePath}${filename}.${testConstants.extension}`;
 
       /* Check file exists */
       if (!fs.existsSync(expectedPath))
@@ -72,15 +74,19 @@ describe('Storage adapter', () => {
 
       /* If data.filename is of type string */
       expect(typeof data.filename === 'string').toBeTruthy();
+
       /* If parsed data.size of type number */
       expect(typeof parseInt(data.size) === 'number').toBeTruthy();
+
       /* If data.mime_type split bigger than 0 and less than 2.
          I.e. application/json => [application, json] is bigger
          than 0 and less than 2 */
       expect(() => {
         const mimeTypeLen = data.mime_type.split('/').length;
+
         return mimeTypeLen > 0 && mimeTypeLen < 2;
       }).toBeTruthy();
+
       /* data.path is of type string */
       expect(typeof data.path === 'string').toBeTruthy();
     });
