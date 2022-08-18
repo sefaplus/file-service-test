66e7f58c9569bb10eab973cc916a3112030c78b1
GeorgiyI <izmailov.georgiy.fortech@gmail.com>
2022-08-18 11:33:06 +0300

    env fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,9 +6,10 @@ export const config = {
     type: process.env.STORAGE_TYPE as StorageTypes,
     maxFileSizeBytes: parseInt(process.env.MAX_FILE_LIMIT || '1') * 1048576, // 1mb === 1048576;
     localSavePath: process.env.LOCAL_SAVE_PATH,
-    mongoAccessUrl: parseInt(process.env.USING_DOCKER_COMPOSE || '0')
-      ? `mongodb://${process.env.MONGO_ROOT_USER}:${process.env.MONGO_ROOT_PASS}@localhost:27017/`
-      : ``,
+    mongoAccessUrl:
+      (parseInt(process.env.USING_DOCKER_COMPOSE || '0')
+        ? `mongodb://${process.env.MONGO_ROOT_USER}:${process.env.MONGO_ROOT_PASS}@localhost:27017/`
+        : process.env.MONGO_CLOUD_URL) || '',
     mongoDbName: process.env.MONGO_DB_NAME || 'FilesDb',
   },
   server: {
